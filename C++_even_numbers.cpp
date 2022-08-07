2f3997e3a6dbeff858a25712983bb6f9ac017340
SK-Githubtest <102689094+SK-Githubtest@users.noreply.github.com>
2022-08-07 22:03:18 +0000

    Add files via upload

---
+++ C++_even_numbers.cpp
@@ -0,0 +1,21 @@
+/*A simple code to calculate the sum of all even numbers less than a number*/
+#include <iostream>
+using namespace std;
+
+int main (){
+    int num1;
+    int i = 1;
+    int sum = 0;
+
+    cout << "enter a number: ";
+    cin >> num1;
+
+    while (i < num1){
+        if (i%2 == 0){
+            sum = sum + i;
+        }
+    i ++;
+    }
+
+    cout <<"The sum of even numbers less than " <<num1<< " is: " <<sum;
+}
